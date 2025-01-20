746b7097192ee8fa6c7aea1393acbd51aafa6f4e
brycebien <brycebien@gmail.com>
2025-01-20 13:59:01 -0500

    map opens to nku's campus

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,8 +9,18 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  static const LatLng _center = LatLng(39.03, -84.46);
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 16.0,
+        ),
+      ),
+    );
   }
 }
